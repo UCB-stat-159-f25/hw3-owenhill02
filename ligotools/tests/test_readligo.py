3782717d906e372d5987fe2d2ab318d5e7602d81
Owen Hill <owen.hill03@gmail.com>
2025-11-09 01:31:34 +0000

    Homework Progress

---
+++ ligotools/tests/test_readligo.py
@@ -0,0 +1,8 @@
+from ligotools import read_frame
+from ligotools import read_hdf5
+
+def test_read_frame():
+    assert 1 == True
+
+def test_read_hdf5():
+	assert 1 == True
---
+++ ligotools/tests/test_utils.py
@@ -0,0 +1,8 @@
+from ligotools.utils import whiten
+from ligotools.utils import write_wavfile
+
+def test_whiten():
+    assert 1 == True
+
+def test_write_wavfile():
+	assert 1 == True
